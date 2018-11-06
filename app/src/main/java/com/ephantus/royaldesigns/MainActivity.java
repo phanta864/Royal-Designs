c2f15b2a5f2640f87baf86656a0f4f03f320713b
phanta864 <ephantusmwangi864@gmail.com>
2018-11-06 16:07:23 +0300

    new button id

--- app/src/main/java/com/ephantus/royaldesigns/MainActivity.java
+++ app/src/main/java/com/ephantus/royaldesigns/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
         mWelcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
