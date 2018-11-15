3e885185572b53debe4475e94aa5faa7e54669f1
phanta864 <ephantusmwangi864@gmail.com>
2018-11-15 13:34:33 +0300

    intergrating json

--- app/src/main/java/com/ephantus/royaldesigns/Signup.java
+++ app/src/main/java/com/ephantus/royaldesigns/Signup.java
@@ -1,13 +1,40 @@
 package com.ephantus.royaldesigns;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Signup extends AppCompatActivity {
+    @BindView(R.id.loginButton) Button mSignupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signup.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
+
+
+
+
+
+
+
+
