2f81857fda9114c0a7f93c521e28c8ba9bacb92b
phanta864 <ephantusmwangi864@gmail.com>
2018-11-15 13:32:52 +0300

    adding service class

--- app/src/main/java/com/ephantus/royaldesigns/FashionService.java
+++ app/src/main/java/com/ephantus/royaldesigns/FashionService.java
@@ -1,5 +1,7 @@
 package com.ephantus.royaldesigns;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,13 +21,17 @@ public class FashionService {
     public static void findFashion(Callback callback){
         OkHttpClient client = new OkHttpClient();
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.FASHION_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter("client_id", Constants.FASHION_API_KEY)
+                .addQueryParameter("query", "clothing");
         String url = urlBuilder.build().toString();
+        Log.d("URL", url);
 
         Request request= new Request.Builder()
-                .addHeader("X-APP-ID",Constants.FASHION_APP_ID)
-                .addHeader( "X-API-KEY" ,Constants.FASHION_API_KEY)
+                .addHeader("Authorization", "Client-ID " + Constants.FASHION_API_KEY)
                 .url(url)
                 .build();
+        String lastRequest= request.toString();
+        Log.d("WesAme",lastRequest);
 
         Call call= client.newCall(request);
         call.enqueue(callback);
@@ -37,15 +43,25 @@ public class FashionService {
         try {
             String jsonData= response.body().string();
             if (response.isSuccessful()){
-                JSONObject fashionJSON = new JSONObject(jsonData);
-                JSONArray outputJSON = fashionJSON.getJSONArray("outputs");
-                JSONObject labels= outputJSON.getJSONObject("labels");
+//                Log.i("ephaDesignsHello",jsonData);
+                JSONArray fashionJSON = new JSONArray(jsonData);
+                for (int i=0; i<fashionJSON.length(); i++){
+                    JSONObject imageJSON = fashionJSON.getJSONObject(i);
+                    JSONObject category= imageJSON.getJSONObject("users");
+                    String name= category.getString("id");
+
+
+                    Fashion fashion= new Fashion(name);
+                    fashions.add(fashion);
+                }
             }
-        }catch (IOException e){
-            e.printStackTrace();
-        }catch (JSONException e){
+        }
+        catch (IOException e){
             e.printStackTrace();
         }
-
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+        return fashions;
     }
 }
