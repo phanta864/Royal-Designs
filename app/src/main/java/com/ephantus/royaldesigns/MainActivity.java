2c8e943780fdcddd954a8d7b312e7d0bcbff6571
phanta864 <ephantusmwangi864@gmail.com>
2018-11-15 13:40:06 +0300

    solving api error

--- app/src/main/java/com/ephantus/royaldesigns/MainActivity.java
+++ app/src/main/java/com/ephantus/royaldesigns/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ephantus.royaldesigns;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,21 +12,38 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.findRoyalDesignButton) Button mWelcomeButton;
-    //    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.findLogInButton) Button mLoginButton;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
-
-
+    @BindView(R.id.findSignUpButton) Button mSignUpButton;
+    @BindView(R.id.mottoEntry) TextView mMottoEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mWelcomeButton.setOnClickListener(new View.OnClickListener() {
+        Typeface logoFonts = Typeface.createFromAsset(getAssets(), "fonts/Black_Ops_One/BlackOpsOne-Regular.ttf");
+        mAppNameTextView.setTypeface(logoFonts);
+
+        Typeface robsterFonts = Typeface.createFromAsset(getAssets(), "fonts/Rochester/Rochester-Regular.ttf");
+        mMottoEntry.setTypeface(robsterFonts);
+
+
+
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Signup.class);
                 startActivity(intent);
             }
         });
