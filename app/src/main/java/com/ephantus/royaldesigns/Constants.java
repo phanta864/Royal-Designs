2b23af6df778c04e5b19ac80afd32051cfc9dafa
phanta864 <ephantusmwangi864@gmail.com>
2018-11-15 12:35:40 +0300

    creating sing up form

--- app/src/main/java/com/ephantus/royaldesigns/Constants.java
+++ app/src/main/java/com/ephantus/royaldesigns/Constants.java
@@ -3,5 +3,5 @@ package com.ephantus.royaldesigns;
 public class Constants {
     public static final String FASHION_API_KEY = BuildConfig.FASHION_API_KEY;
     public static final String FASHION_APP_ID = BuildConfig.FASHION_APP_ID;
-    public static final String FASHION_BASE_URL = "https://jsonplaceholder.typicode.com/photos";
+    public static final String FASHION_BASE_URL = "https://api.unsplash.com/search/photos";
 }
